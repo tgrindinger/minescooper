81a4996dcf2889939d1541ffbc93209f30ffecbe
Thomas Grindinger <thomas@linux-ei28.site>
2016-05-04 12:14:20 -0500

    update dependencies

--- gulp/tasks/browserify.js
+++ gulp/tasks/browserify.js
@@ -8,17 +8,19 @@ var source       = require('vinyl-source-stream');
 gulp.task('browserify', function() {
 	var bundleMethod = global.isWatching ? watchify : browserify;
 
-	var bundler = bundleMethod({
+        var props = {
 		entries: ['./source/javascripts/minegame.coffee'],
-		extensions: ['.coffee']
-	});
+		extensions: ['.coffee'],
+                debug: true,
+                cache: {},
+                packageCache: {}
+        };
+        var bundler = global.isWatching ? watchify(browserify(props)) : browserify(props);
 
 	var bundle = function() {
 		bundleLogger.start();
-
-		return bundler
-			.bundle({debug: true})
-			.on('error', handleErrors)
+                var stream = bundler.bundle();
+                return stream.on('error', handleErrors)
 			.pipe(source('compiled.js'))
 			.pipe(gulp.dest('./build'))
 			.on('end', bundleLogger.end);
