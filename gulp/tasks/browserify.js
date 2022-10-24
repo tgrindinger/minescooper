10bb8c134a172c7487cfdb77623736942995ea40
Thomas Grindinger <tgrindinger@gmail.com>
2022-10-23 23:46:51 -0400

    add github action to deploy

--- gulp/tasks/browserify.js
+++ gulp/tasks/browserify.js
@@ -6,29 +6,24 @@ var handleErrors = require('../util/handleErrors');
 var source       = require('vinyl-source-stream');
 
 gulp.task('browserify', function() {
-	var bundleMethod = global.isWatching ? watchify : browserify;
-
-        var props = {
+	var props = {
 		entries: ['./source/javascripts/minegame.coffee'],
 		extensions: ['.coffee'],
-                debug: true,
-                cache: {},
-                packageCache: {}
-        };
-        var bundler = global.isWatching ? watchify(browserify(props)) : browserify(props);
-
+		debug: true,
+		cache: {},
+		packageCache: {}
+	};
+	var bundler = global.isWatching ? watchify(browserify(props)) : browserify(props);
 	var bundle = function() {
 		bundleLogger.start();
-                var stream = bundler.bundle();
-                return stream.on('error', handleErrors)
+		var stream = bundler.bundle();
+		return stream.on('error', handleErrors)
 			.pipe(source('compiled.js'))
 			.pipe(gulp.dest('./build'))
 			.on('end', bundleLogger.end);
 	};
-
 	if (global.isWatching) {
 		bundler.on('update', bundle);
 	}
-
 	return bundle();
 });
